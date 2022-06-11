70853ca88cca5dd8dd9446be7d236e555d3cd62e
Iman Salehi <cse.isalehi@gmail.com>
2022-06-11 17:58:27 +0430

    poor nessy

---
+++ src/SearchingForNessy.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// 11044 - Searching for Nessy
+public class SearchingForNessy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCase = scanner.nextInt();
+
+        while (testCase > 0) {
+
+            int rows = scanner.nextInt();
+            int columns = scanner.nextInt();
+
+            rows /= 3;
+            columns /= 3;
+
+            System.out.println(rows*columns);
+
+            testCase--;
+        }
+    }
+
+}
