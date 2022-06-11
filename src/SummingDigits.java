6f32f6610dac072eb9407bc9e9d502f6852b5512
Iman Salehi <cse.isalehi@gmail.com>
2022-06-11 17:57:52 +0430

    use a method

---
+++ src/SummingDigits.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// 11332 - Summing Digits
+public class SummingDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int n = scanner.nextInt();
+            if (n == 0) {
+                break;
+            }
+            System.out.println(sumOfDigits(n));
+        }
+    }
+
+    private static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10);
+            n /= 10;
+            if (n == 0 && sum > 9) {
+                n = sum;
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+}
