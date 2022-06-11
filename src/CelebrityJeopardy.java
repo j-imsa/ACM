8c1c30dc74b9f11c5fd64ab75ecc47ef4554a318
Iman Salehi <cse.isalehi@gmail.com>
2022-06-11 17:52:26 +0430

    another dummy problem

---
+++ src/CelebrityJeopardy.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// 1124 - Celebrity jeopardy
+public class CelebrityJeopardy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
