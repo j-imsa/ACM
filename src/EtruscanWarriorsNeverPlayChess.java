5ec8c8d5152ec4101488e2bd0324d1c01038eea2
Iman Salehi <cse.isalehi@gmail.com>
2022-06-11 17:57:24 +0430

    math problem

---
+++ src/EtruscanWarriorsNeverPlayChess.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// 11614 - Etruscan Warriors Never Play Chess
+public class EtruscanWarriorsNeverPlayChess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int testCase = scanner.nextInt();
+
+
+        while (testCase > 0) {
+
+            long warriors = scanner.nextLong();
+
+            System.out.println((long) Math.floor(0.5 + Math.sqrt(2.0 * warriors + 0.25)) - 1);
+
+            testCase--;
+        }
+    }
+
+}
