6d13335b6154fd1b5cd4208c4eb5f7942c9cc1c6
Iman Salehi <cse.isalehi@gmail.com>
2022-06-17 17:49:51 +0430

    add both, minToMax and maxToMin

--- src/AmalgamatedArtichokes.java
+++ src/AmalgamatedArtichokes.java
@@ -21,14 +21,47 @@ public class AmalgamatedArtichokes {
                 prices[i] = p * (Math.sin(a * (i + 1) + b) + Math.cos(c * (i + 1) + d) + 2);
             }
 
+            int maxIndex = findMaxIndex(prices);
+            int minIndexAfterMax = findMinIndexAfterMax(prices, maxIndex);
+            double maxToMin = prices[maxIndex] - prices[minIndexAfterMax];
+
             int minIndex = findMinIndex(prices);
             int maxIndexBeforeMin = findMaxIndexBeforeMin(prices, minIndex);
-            double result = prices[maxIndexBeforeMin] - prices[minIndex];
-            System.out.printf("%.6f\n", result);
+            double minToMax = prices[maxIndexBeforeMin] - prices[minIndex];
+
+            if (maxToMin > minToMax) {
+                System.out.printf("%.6f\n", maxToMin);
+            } else {
+                System.out.printf("%.6f\n", minToMax);
+            }
 
 
         }
 
+    }
+
+    public static int findMaxIndex(double[] arr) {
+        double max = arr[0];
+        int maxIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public static int findMinIndexAfterMax(double[] arr, int maxIndex) {
+        double min = arr[maxIndex];
+        int minIndex = maxIndex;
+        for (int i = maxIndex; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 
     public static int findMinIndex(double[] arr) {
