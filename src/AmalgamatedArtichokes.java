aabaf11f395834be22ba708c541e98ec282a536b
Iman Salehi <cse.isalehi@gmail.com>
2022-06-17 18:04:45 +0430

    calc distance between two points

--- src/AmalgamatedArtichokes.java
+++ src/AmalgamatedArtichokes.java
@@ -21,70 +21,22 @@ public class AmalgamatedArtichokes {
                 prices[i] = p * (Math.sin(a * (i + 1) + b) + Math.cos(c * (i + 1) + d) + 2);
             }
 
-            int maxIndex = findMaxIndex(prices);
-            int minIndexAfterMax = findMinIndexAfterMax(prices, maxIndex);
-            double maxToMin = prices[maxIndex] - prices[minIndexAfterMax];
-
-            int minIndex = findMinIndex(prices);
-            int maxIndexBeforeMin = findMaxIndexBeforeMin(prices, minIndex);
-            double minToMax = prices[maxIndexBeforeMin] - prices[minIndex];
-
-            if (maxToMin > minToMax) {
-                System.out.printf("%.6f\n", maxToMin);
-            } else {
-                System.out.printf("%.6f\n", minToMax);
+            double max = prices[0];
+            double decline = 0.0;
+            for (int i = 1; i < prices.length; i++) {
+                if (max - prices[i] > 0) {
+                    if ((max - prices[i]) > decline) {
+                        decline = max - prices[i];
+                    }
+                } else {
+                    max = prices[i];
+                }
             }
 
+            System.out.printf("%.6f\n", decline);
 
         }
 
     }
 
-    public static int findMaxIndex(double[] arr) {
-        double max = arr[0];
-        int maxIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= max) {
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-
-    public static int findMinIndexAfterMax(double[] arr, int maxIndex) {
-        double min = arr[maxIndex];
-        int minIndex = maxIndex;
-        for (int i = maxIndex; i < arr.length; i++) {
-            if (arr[i] <= min) {
-                min = arr[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
-
-    public static int findMinIndex(double[] arr) {
-        double min = arr[0];
-        int minIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= min) {
-                min = arr[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
-
-    public static int findMaxIndexBeforeMin(double[] arr, int minIndex) {
-        double max = arr[minIndex];
-        int maxIndex = minIndex;
-        for (int i = 0; i < minIndex; i++) {
-            if (arr[i] >= max) {
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
 }
