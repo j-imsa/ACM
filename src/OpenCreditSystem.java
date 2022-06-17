98e4c2d81146d5278702b355423e2d4096845f66
Iman Salehi <cse.isalehi@gmail.com>
2022-06-17 21:12:22 +0430

    solved

--- src/OpenCreditSystem.java
+++ src/OpenCreditSystem.java
@@ -13,40 +13,22 @@ public class OpenCreditSystem {
                 scores[i] = scanner.nextInt();
             }
 
-            int maxSeniorIndex = maxSeniorIndex(scores);
-            int maxSenior = scores[maxSeniorIndex];
+            int pointA = scores[0];
+            int deference = Integer.MIN_VALUE;
+            for (int i = 1; i < scores.length; i++) {
+                int pointB = scores[i];
+                if (pointA - pointB > deference) {
+                    deference = pointA - pointB;
+                }
+                if (pointB > pointA) {
+                    pointA = pointB;
+                }
+            }
 
-            int minJunior = minJunior(scores, maxSeniorIndex);
-
-            System.out.println(maxSenior - minJunior);
+            System.out.println(deference);
 
             testCase--;
         }
     }
 
-    public static int maxSeniorIndex(int[] arr) {
-        // ignore the last element
-        int max = arr[0];
-        int maxIndex = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] >= max) {
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-
-    public static int minJunior(int[] arr, int maxIndex) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (i <= maxIndex) {
-                continue;
-            }
-            if (arr[i] <= min) {
-                min = arr[i];
-            }
-        }
-        return min;
-    }
 }
